7adc8f8dde3946f919e7561b71c82ff537cfee3a
Gorchon <a01254831@tec.mx>
2024-06-06 23:15:43 -0600

    dev: RAG controller

--- server_start/routes/chatOpenRoutes.js
+++ server_start/routes/chatOpenRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
+// const ragController = require('../controllers/RagController');
+
 
 
 const geminiController = require('../controllers/geminiController');
 router.post ('/chat', chatController.getResponseChat);
 router.post('/gemini', geminiController.getResponseChatGemini);
+// router.post('/context', ragController.getContextResponse);
+
 
 module.exports = router; 
